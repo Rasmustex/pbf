150380eedd724bed17a907c674be9b0493289d12
Rasmus Hauge Hansen <rasmus14326@gmail.com>
2021-09-06 18:10:19 +0200

    better looking file opening code

--- src/filehandling.c
+++ src/filehandling.c
@@ -7,10 +7,11 @@
 bfFile* getbffile( const char* filename ) {
     bfFile* file = (bfFile*)malloc( sizeof( bfFile ) );
     FILE *f;
-    if( access( filename, F_OK ) == 0 )
-        f = fopen(filename, "rb" ); // open the file
-    else
-        printError( "Couldn't access the given file", fileOpeningError );
+    // if( access( filename, F_OK ) == 0 )
+    //     f = fopen(filename, "rb" ); // open the file
+    // else
+    //     printError( "Couldn't access the given file", fileOpeningError );
+    access( filename, F_OK ) ? printError( "Couldn't access the given file", fileOpeningError ) : (f = fopen(filename, "rb" ));
 
     fseek( f, 0, SEEK_END ); // go to the end of the file stream
     long fsize = ftell(f); // get current position in the file stream. As we are at the end, this will be the final character
